d6d2d9aa7923458db86cd74de6504fe76c421921
Jeong Kiyeon <ions@kakao.com>
2023-08-03 12:41:21 +0900

    초기 커밋.

---
+++ lottoSellecter/Form1.cs
@@ -0,0 +1,17 @@
+using Microsoft.ML;
+
+namespace lottoSellecter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void initMl()
+        {
+
+        }
+    }
+}
---
+++ lottoSellecter/Program.cs
@@ -0,0 +1,16 @@
+namespace lottoSellecter
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            new CycleML();
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
